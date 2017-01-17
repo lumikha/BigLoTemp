99305af89a0b6e9f0525a838167564c23bfeaccc
Bethany Joyce Sibala <bethany@lumikha.co>
2017-01-17 11:29:07 +0800

    unfinished
    not yet done.

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> BigLo</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/">
+	<link rel="stylesheet" type="text/css" href="style.css"><!--main stylesheet-->
+
+	 <!--media queries-->
+    <link rel="stylesheet" type="text/css" href="assets/css/mobile_small.css"> <!--320x480-->
+    <link rel="stylesheet" type="text/css" href="assets/css/small_tablet.css"> <!--480x640-->
+     <link rel="stylesheet" type="text/css" href="assets/css/tablet_portrait.css"> <!--768x1024-->
+
+
+</head>
+<body>
+
+<div class="main"> <!--contains everything; rocketship background-->
+	<div class="header"> <!--.header-->
+		<div class="logo"> <!--b logo-->
+		<br>
+		<p> <a href=""><img src="/BigloTemp/assets/img/logo" id="logo_b"> </a> </p><!--change this to separate images-->
+		</div> <!--end of logo-->
+
+		<div class="header_details"> <!--header text with <h1> to <h6>tags-->
+			<div class="h1"> Big Local is Taking Off  </div>  
+			<div class="h2"> 
+
+				<p><div id="word1">Business Website </div> 
+				<div id="plus1"> &nbsp; + </div>
+				<div id="word2"> &nbsp; Actionable Reporting </div> 
+				<div id="plus2"> &nbsp; + </div> 
+				<div id="word3"> &nbsp; Local Optimization</div>
+				</p>
+			</div> 
+			
+			<div class="caret_hidden"><p><a href=""><img src="/BigloTemp/assets/img/down-arrow" id="caret"> </a> </p> </div><!--down caret-->
+
+			<div class="h3"> 
+							<p><div id="word1"> Get a beta account today &nbsp;  </div> 
+							<div id="word2"> and help us make Big Local &nbsp;</div>
+							<div id="word3"> bigger and localler. &nbsp; </div> </br> 
+							</p>
+							
+							<p><div id="word1"> Beta Subscription:&nbsp; </div>
+							<div id="word2"> $4.99/month. &nbsp; </div>
+							<div id="word3"> Forever. </div> </p>
+			</div> <br> <br> 
+			
+		</div> <!--end of header_details-->
+
+		<p><a href=""><img src="/BigloTemp/assets/img/down-arrow" id="caret"> </a> </p><!--down caret-->
+
+	</div> <!--end of .header--> 
+
+	<div class="middle_transparent"> <!--main content; contains the form and the iphone image-->
+
+	</div> <!--end of middle_transparent-->
+
+	<div class="middle_content">		
+		<div class="phone_image" > <!--iphone image left part of the middle container-->
+			<img src="/BigloTemp/assets/img/iphone" id="iphone">
+		</div> <!--end of iphone image-->
+
+		<div class="form-group"> <!--form sign in form-->
+			
+			<form action="">
+				<div class="form_title"> Let's Do This </div> <br> <br> <br>
+
+ 				<input type="text" class="name" name="Name" placeholder="Name*" style="width: 166px;"> &nbsp; &nbsp; &nbsp;
+ 				<input type="text" class="email" name="name" placeholder="Email*" style="width: 166px;">
+ 				<br><br><br>
+ 				<input type="text" class="message" name="Message" placeholder="Message*" style="width: 360px;">
+  				<br> <br>
+  				<button type="SUBMIT" value="SUBMIT"> SUBMIT </button>
+
+			</form>
+		</div> <!--end of form group-->	
+	</div> <!--end of middle_content-->
+
+
+
+	<div class="footer"> <!--footer-->
+		<p>&copy 2016 Atelier Lumikha</p>
+	</div> <!--end of footer-->
+</div> <!--end of .main-->
+
+</body>
+</html>
+
