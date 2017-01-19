d99dbcc19dc09539e449220657b89c6dfd2d873f
Bethany Joyce Sibala <bethany@lumikha.co>
2017-01-19 13:36:21 +0800

    success page with a simple layout
    :dancers:

--- success.php
+++ success.php
@@ -2,15 +2,21 @@
 <html>
 <head>
 	<title> Message Sent</title>
-	
+
+	<link rel="Shortcut icon" href="assets/img/favicon_logo.ico"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="success_style.css"><!--main stylesheet-->
 </head>
+
 <body>
 
-<style>
-<?php include '/style.css'; ?>
-</style>
+<div class="main">
+	<div class="h1"> <p> Your Message has been successfully sent. Thank You! </p> </div>
 
-<h1> Message has been successfully sent </h1>
+	<div class="back_button"><p><a href="index.php"><button type="button" name="back" > Back to Main Page </button></a></p> </div> 
+
+</div>
+
 
 </body>
 
