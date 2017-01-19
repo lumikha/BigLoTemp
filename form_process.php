55c4f5d6d179f2fafd6fc04a6816795438c8f465
Bethany Joyce Sibala <bethany@lumikha.co>
2017-01-19 14:10:22 +0800

    DONE! Live and Running. <3
    PTL! :dancer:

--- form_process.php
+++ form_process.php
@@ -1,11 +1,11 @@
 <?php 
 
 	if(isset($_POST['submit'])){
-    $to = "bethanyjoycesibala@gmail.com"; // this is your Email address
+    $to = "harry@lumikha.co"; // this is your Email address
     $from = $_POST['Email']; // this is the sender's Email address
     $Name = $_POST['Name'];
     $subject = "Form submission from Big Local";
-    $Message = $Name . " " . $from . " wrote the following:" . "\n\n" . $_POST['Message'];
+    $Message = $Name . " " . " wrote the following:" . "\n\n" . $_POST['Message'];
     $headers = "From:" . $from;
     $checkMail = mail($to,$subject,$Message,$headers);
    
